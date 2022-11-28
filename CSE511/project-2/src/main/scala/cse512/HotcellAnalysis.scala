4cef3d8b54c618e5c1f1664e7dd172a0b642ea8d
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-11-28 22:53:35 +0900

    CSE511: edit hotcellanalysis functions

--- CSE511/project-2/src/main/scala/cse512/HotcellAnalysis.scala
+++ CSE511/project-2/src/main/scala/cse512/HotcellAnalysis.scala
@@ -42,8 +42,58 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
     val maxZ = 31
     val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
 
-    // YOU NEED TO CHANGE THIS PART
+    // create temporary View of pickupInfo
+    pickupInfo.createOrReplaceTempView("pickupInfoView")
 
-    return pickupInfo // YOU NEED TO CHANGE THIS PART
+    // define function to check if point is inside the cube boundary.
+    spark.udf.register("inBounds", (x: Double, y: Double, z: Int) =>  (x >= minX) && (x <= maxX) && (y >= minY) && (y <= maxY) && (z >= minZ) && (z <= maxZ))
+
+    // get x, y, z points which are inside boundary
+    val filteredPointsDf = spark.sql("select x, y, z from pickupInfoView where inBounds(x, y, z) order by z, y, x").persist()
+    filteredPointsDf.createOrReplaceTempView("filteredPointsView")
+
+    // count all the pickups from same cell
+    val filteredPointCountDf = spark.sql("select x, y, z, count(*) as numPoints from filteredPointsView group by z, y, x order by z, y, x").persist()
+    filteredPointCountDf.createOrReplaceTempView("filteredPointCountDfView")
+
+    // define function square
+    spark.udf.register("square", (inputX: Int) => (inputX*inputX).toDouble)
+    val sumOfPoints = spark.sql("select count(*) as numCellsWithAtleastOnePoint, sum(numPoints) as totalPointsInsideTheGivenArea, sum(square(numPoints)) as squaredSumOfAllPointsInGivenArea from filteredPointCountDfView")
+    sumOfPoints.createOrReplaceTempView("sumOfPoints")
+
+    // calculate values required for G Score
+    val totalPoints = sumOfPoints.first().getLong(1)
+    val squaredSumCells = sumOfPoints.first().getDouble(2)
+    val xBar = totalPoints / numCells
+    val SD = math.sqrt((squaredSumCells / numCells) - (xBar * xBar) )
+
+    // query neighbours based on counts
+    spark.udf.register("GetNeighbourCount", (minX: Int, minY: Int, minZ: Int, maxX: Int, maxY: Int, maxZ: Int, Xin: Int, Yin: Int, Zin: Int)
+    => ((HotcellUtils.getNeighbourCount(minX, minY, minZ, maxX, maxY, maxZ, Xin, Yin, Zin))))
+    val Neighbours = spark.sql("select " +
+      "view1.x as x, " +
+      "view1.y as y, " +
+      "view1.z as z, " +
+      "GetNeighbourCount("+minX + "," + minY + "," + minZ + "," + maxX + "," + maxY + "," + maxZ + "," + "view1.x,view1.y,view1.z) as totalNeighbours, " +   // function to get the number of neighbours of x,y,z
+      "count(*) as neighboursWithValidPoints, " +  // count the neighbours with atLeast one pickup point
+      "sum(view2.numPoints) as sumAllNeighboursPoints " +
+      "from filteredPointCountDfView as view1, filteredPointCountDfView as view2 " +    // two tables to join
+      "where (view2.x = view1.x+1 or view2.x = view1.x or view2.x = view1.x-1) and (view2.y = view1.y+1 or view2.y = view1.y or view2.y = view1.y-1) and (view2.z = view1.z+1 or view2.z = view1.z or view2.z = view1.z-1) " +   //join condition
+      "group by view1.z, view1.y, view1.x order by view1.z, view1.y, view1.x").persist()
+
+    Neighbours.createOrReplaceTempView("NeighboursView")
+
+    spark.udf.register("GetGScore", (x: Int, y: Int, z: Int, numCells: Int, mean:Double, sd: Double, totalNeighbours: Int, sumAllNeighboursPoints: Int) => ((
+      HotcellUtils.getGScore(x, y, z, numCells, mean, sd, totalNeighbours, sumAllNeighboursPoints))))
+    val NeighboursDesc = spark.sql("select x, y, z, " +
+      "GetGScore(x, y, z," + numCells + ", " + xBar + ", " + SD + ", totalNeighbours, sumAllNeighboursPoints) as gi_statistic " +
+      "from NeighboursView " +
+      "order by gi_statistic desc")
+
+    NeighboursDesc.createOrReplaceTempView("NeighboursDescView")
+    NeighboursDesc.show()
+    val hotcells_dec_order = spark.sql("select x, y, z from NeighboursDescView")
+
+    hotcells_dec_order
   }
 }
